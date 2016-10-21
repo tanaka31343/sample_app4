679883f8a2cec304d6cfe7f7a997e6481904add6
takuya_kimura <sunaosaimyo@gmail.com>
2016-10-21 07:09:02 +0100

    2 failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'help' => 'static_pages#help'
   root 'static_pages#home'
   get 'signup'  => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  post 'users' => 'users#create'
+  resources :users
 end
